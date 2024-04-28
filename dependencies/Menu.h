685bc32e46cdda3a5868d74913987b345212336b
hamail sheikh <hamailsheikh1@gmail.com>
2024-04-29 00:06:31 +0500

    Semaphores added to Factory.h

--- dependencies/Menu.h
+++ dependencies/Menu.h
@@ -12,13 +12,16 @@ using namespace sf;
 struct Menu
 {
     RectangleShape menu;
+    RenderWindow *window;
 
     Texture cone_t;
     Sprite cone_s[3];
 
     Menu()
     {
-        cone_t.loadFromFile("assets/images/cone.png");
+        window = new RenderWindow(VideoMode(1600, 900), "Data Structure Visualizer", Style::Titlebar | Style::Close);
+    
+        // cone_t.loadFromFile("assets/images/cone.png");
 
         for (int i = 0; i < 3; i++)
             cone_s[i].setTexture(cone_t);
@@ -30,21 +33,48 @@ struct Menu
         cone_s[0].setPosition(448.8, 398.8);
         cone_s[1].setPosition(736, 386);
         cone_s[2].setPosition(1023.2, 373.2);
+        cone_s[1].setColor(Color::Red);
+        cone_s[2].setColor(Color::Blue);
+        
 
-        menu.setOutlineThickness(-4.f);
-        menu.setFillColor(Color::White);
-        menu.setSize(Vector2f(900, 300));
-        menu.setOutlineColor(Color::Black);
-        menu.setPosition(Vector2f(350, 300));
+        float w = window->getSize().x;
+        int h = window->getSize().y;
+
+        menu.setSize(Vector2f(w, h));
+        menu.setFillColor(Color(100, 100,  100));
+        
+
+        
     }
 
-    void draw(RenderWindow *window)
+    void draw()
     {
+        window->clear();
         window->draw(menu);
         window->draw(cone_s[0]);
         window->draw(cone_s[1]);
         window->draw(cone_s[2]);
+         Event event;
+    while (window->pollEvent(event))
+    {
+        if (event.type == Event::MouseButtonPressed)
+        {
+            
+            Vector2f mousePos = window->mapPixelToCoords(Mouse::getPosition(*window));
+
+            
+            for (int i = 0; i < 3; i++)
+            {
+                if (cone_s[i].getGlobalBounds().contains(mousePos))
+                {
+                    
+                    cone_s[i].setColor(Color::Red); 
+                }
+            }
+        }
+    }
+
+        window->display();
     }
 };
-
 #endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "dependencies/Visualizer.h"
 
+
+
 int main()
 {
+    Menu men;
+    men.draw();
     Visualizer viz;
     viz.setup();
     viz.run();
